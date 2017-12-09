200200eb152a147eb3fb53b20deff6a7074af030
Ayaki Tamura <ayaki067@gmail.com>
2017-12-09 02:28:02 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SanmpleApp::Application.config.secret_key_base = '37f44bfc7c1565e82c538f615718ac437b541354fcaedd96d54e2d9af98745e3b8ecf06791e18b07c51d957c40fe1f00a1b98a5b727be7f2baeb9f7b70248c38'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SanmpleApp::Application.config.session_store :cookie_store, key: '_sanmple_app_session'
